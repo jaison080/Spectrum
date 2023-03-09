d10c76e62c303ef171b6e6a7cb7b22f102f79d64
hannasalam <hannasalam113@gmail.com>
2023-03-09 07:43:30 +0530

    user login route completed

---
+++ backend/controllers/user/login.js
@@ -0,0 +1,33 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+
+    try {
+      const { email, password } = req.body;
+  
+      if (!(email && password)) {
+        res.status(400).send("All input is required");
+      }
+      const user = await User.findOne({ email });
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "2h",
+          }
+        );
+        user._doc.token = token;
+        return res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credentials");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+module.exports = { loginUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/user/userRegister')
+const { registerUser } = require('../controllers/user/register')
+const { loginUser } = require('../controllers/user/login')
 
 // Base URL: /api/users
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router
