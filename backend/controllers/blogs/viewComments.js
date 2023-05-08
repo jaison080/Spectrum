84ab929b272f9b8487f198ad58828d003e4be8fc
AMAL <mailtoamaldevcd@gmail.com>
2023-05-08 09:01:29 +0530

    add comments & view comments done

---
+++ backend/controllers/blogs/viewComments.js
@@ -0,0 +1,21 @@
+const Blogs = require("../../models/blogs");
+const viewComments = async (req,res) =>
+{
+    try{
+        const result = await Blogs.find(
+            {
+                _id: req.params.id,
+            },
+            {
+                comments:1
+            }
+        );
+        res.status(200).send(result);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+}
+
+module.exports = {viewComments}
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -6,9 +6,12 @@ const {getBlogsByUser} = require('../controllers/blogs/getBlogsByUser')
 const {createBlog} = require('../controllers/blogs/createBlog')
 const {editBlog} = require('../controllers/blogs/editBlogs')
 const {likeBlog} = require('../controllers/blogs/likeBlog')
+const {addComment} = require('../controllers/blogs/addComment')
+const {viewComments} = require('../controllers/blogs/viewComments');
 const {verifyToken} = require('../middlewares/userAuth')
 const storage = require('../utils/cloudinary');
 const multer = require('multer');
+const { verify } = require('jsonwebtoken')
 
 const upload = multer({ storage: storage });
 
@@ -19,5 +22,6 @@ router.get('/user', verifyToken, getBlogsByUser);
 router.post('/create', verifyToken, upload.single('image'), createBlog);
 router.put('/edit',verifyToken, editBlog);
 router.put('/like/:id',verifyToken,likeBlog);
-
+router.put('/addcomment/:id',verifyToken,addComment);
+router.get('/comment/:id',verifyToken,viewComments)
 module.exports = router
