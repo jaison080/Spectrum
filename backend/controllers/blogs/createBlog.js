8ff7a75dfa4585cda55198e9bef3c88833f43115
AMAL <mailtoamaldevcd@gmail.com>
2023-05-22 20:50:33 +0530

    fix:like unique error

--- backend/controllers/blogs/createBlog.js
+++ backend/controllers/blogs/createBlog.js
@@ -3,15 +3,17 @@ const cloudinary = require('cloudinary').v2;
 const createBlog = async (req, res) => {
     try{
         const { title, content} = req.body;
-        const result = await cloudinary.uploader.upload(req.file.path);
+        //const result = await cloudinary.uploader.upload(req.file.path);
         const user = req.user.user_id;
         console.log(user);
         const blog = await Blogs.create({
             title: title,
             content: content,
             author: user,
-            image: result.secure_url,
+            likes:[user],
+            //image: result.secure_url,
             });
+            blog.likes.pull(user);
         res.status(200).send(blog);
     }
     catch(err){
