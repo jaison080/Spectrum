f9cadbae8b06d82c502579cc489e28ed0839a8b3
AMAL <mailtoamaldevcd@gmail.com>
2023-06-11 15:58:37 +0530

    login done

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const { registerUser } = require('../controllers/user/register')
 const router = express.Router()
 const registerComp = require('../controllers/jobs/registerComp')
+const loginComp = require('../controllers/jobs/loginComp')
 
 //Base URL: /api/jobs
 router.post('/register',registerComp);
+router.post('/login',loginComp);
 
 module.exports = router
