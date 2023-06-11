5c71d60334369267e66b3b647a8a5ff29163009a
AMAL <mailtoamaldevcd@gmail.com>
2023-06-11 20:55:29 +0530

    add jobs ready

--- backend/models/jobs.js
+++ backend/models/jobs.js
@@ -8,7 +8,9 @@ const  JobsSchema =  new mongoose.Schema({
     prerequisite: [{type: String, required: true }],
     responsibility: [{type: String, required: true}],
     misc: {type: String, required: true},
-    dateOfPosting: {type: Date, default: Date.now}
+    dateOfPosting: {type: Date, default: Date.now},
+    url: {type: String, required: true},
+    postedBy: {type :mongoose.Schema.Types.ObjectId, ref: 'Company', required:true}
 });
 
 module.exports = mongoose.model('Jobs',JobsSchema);
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -3,9 +3,12 @@ const { registerUser } = require('../controllers/user/register')
 const router = express.Router()
 const registerComp = require('../controllers/jobs/registerComp')
 const loginComp = require('../controllers/jobs/loginComp')
+const addJobs = require('../controllers/jobs/addJobs')
+const { verifyToken } = require('../middlewares/userAuth')
 
 //Base URL: /api/jobs
 router.post('/register',registerComp);
 router.post('/login',loginComp);
+router.post('/add',verifyToken,addJobs)
 
 module.exports = router
