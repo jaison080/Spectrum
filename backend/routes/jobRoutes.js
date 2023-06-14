ae06d0730a00ad0f8882831d454b653db902b45e
AMAL <mailtoamaldevcd@gmail.com>
2023-06-14 21:07:46 +0530

    getJobsbyId

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -6,11 +6,13 @@ const loginComp = require('../controllers/jobs/loginComp')
 const addJobs = require('../controllers/jobs/addJobs')
 const { verifyToken } = require('../middlewares/userAuth')
 const getJobs = require('../controllers/jobs/getJobs')
+const getJobsById = require('../controllers/jobs/getJobsById')
 
 //Base URL: /api/jobs
 router.post('/register',registerComp);
 router.post('/login',loginComp);
 router.post('/add',verifyToken,addJobs)
 router.get('/',getJobs)
+router.get('/:id',verifyToken,getJobsById)
 
 module.exports = router
