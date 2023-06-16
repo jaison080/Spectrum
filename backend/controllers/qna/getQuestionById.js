94055727986d161ba0c1bcf4e191e44eceb2752f
hannasalam <hannasalam113@gmail.com>
2023-06-16 11:50:22 +0530

    Fixed bug in answers

--- backend/controllers/qna/getQuestionById.js
+++ backend/controllers/qna/getQuestionById.js
@@ -4,10 +4,10 @@ const getQuestionById = async (req, res) => {
     const question = await Question.findById(req.params.id)
     .populate({
       path: "answers",
-      populate: {
-        path: "answerer",
-        path: "comments.commenter"
-      },
+      populate: [
+        {path: "answerer"},
+        {path: "comments.commenter"}
+      ],
     })
     .populate("author");
     res.status(200).send(question);
