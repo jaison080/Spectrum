76093ff860d9c99b1448647e04f6b28c7d28682f
fayazap <fayazap2@gmail.com>
2023-07-27 22:10:17 +0530

    rainbow symbol for 50k

--- frontend/src/Components/UI/MyComponent.js
+++ frontend/src/Components/UI/MyComponent.js
@@ -6,7 +6,7 @@ const MyComponent = () => {
   return (
     <motion.img
       className="box"
-      src="https://i0.wp.com/pearlyarts.com/wp-content/uploads/2022/03/Rainbow-and-clouds-clipart-with-outline-WM.png?resize=600%2C601&ssl=1"
+      src="images/Logo.png"
       alt="Rotating Image"
       animate={{
         rotate: [0, 0, 360, 360, 0],
